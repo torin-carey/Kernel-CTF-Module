f286bb5a0a107fd23cf3a13cc1e74829e1fe91d1
Torin Carey <torin@tcarey.uk>
2019-03-21 12:43:15 +0000

    Added new 'supplied'
    * Renamed 'supplied' to 'solutions' (Not yet complete)
    * Added 'supplied' with base material

---
+++ supplied/flag_ioctl.h
@@ -0,0 +1,43 @@
+#ifndef H_FLAGIOCTL
+#define H_FLAGIOCTL
+
+#include <linux/ioctl.h>
+
+#ifdef __KERNEL__
+#include <linux/types.h>
+#else
+#include <stdint.h>
+#endif
+
+struct auth_data {
+	// The Message Authentication Code. The SHA-256 hash of the 128-bit key concatenated with the message
+	unsigned char digest[32];
+	// Length of data in message
+	unsigned int message_len;
+	// Null delimited list of commands. Unknown commands will be silently ignored
+	// e.g. 'COMMAND1\0COMMAND2\0UNLOCK_FLAG1'
+	char message[128];
+};
+
+#define MYMAGIC 105
+
+// The null terminated flags will not be longer than this
+#define FLAG_MAX_LEN 64
+
+/* GET_FLAG1 and GET_FLAG2 will fail with EPERM if the appropriate flag hasn't been
+ * unlocked with AUTHENTICATE */
+
+// Does not require any authentication
+#define IOCTL_GET_FLAG1 _IOR(MYMAGIC, 16, char *)
+
+// Requires UNLOCK_FLAG2
+#define IOCTL_GET_FLAG2 _IOR(MYMAGIC, 17, char *)
+
+// Requires UNLOCK_FLAG3
+#define IOCTL_GET_FLAG3 _IOR(MYMAGIC, 18, char *)
+
+/* AUTHENTICATE will fail with EPERM if the message doesn't have
+ * a valid MAC */
+#define IOCTL_AUTHENTICATE _IOW(MYMAGIC, 8, struct auth_data *)
+
+#endif // H_FLAGIOCTL
