933205fd1b5b3c57b0f1808ebb22b0d51b31ffec
Torin Carey <torin@tcarey.uk>
2019-03-21 12:55:08 +0000

    Added gen_flag.sh script for flag.h generation
    * Updated module makefile to auto generate flag.h

---
+++ module/gen_flag.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Simple bash script to produce a 'flag.h' file containing the flags and the key
+
+genflag() {
+	head -c 16 /dev/urandom | xxd -p
+}
+
+KEY=`head -c 16 /dev/urandom | xxd -i`
+
+FLAG1="flag1{`genflag`}"
+FLAG2="flag2{`genflag`}"
+FLAG3="flag3{`genflag`}"
+
+cat <<EOF
+#ifndef H_FLAG
+#define H_FLAG
+
+static unsigned char key[] = {
+$KEY
+};
+
+static const char flag1[] = "$FLAG1";
+static const char flag2[] = "$FLAG2";
+static const char flag3[] = "$FLAG3";
+
+#endif // H_FLAG
+EOF
