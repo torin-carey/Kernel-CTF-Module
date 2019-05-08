bdb223e8d074f3a9f894179b0b25055d438c9a06
Torin Carey <torin@tcarey.uk>
2019-05-08 14:46:38 +0100

    Add RCU instead of atomic state

--- module/ctfmod_main.c
+++ module/ctfmod_main.c
@@ -16,12 +16,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Torin Carey <torin@tcarey.uk>");
 
-#define STATE_UNINIT 0
-#define STATE_READY  1
-
-static atomic_t dev_state = ATOMIC_INIT(STATE_UNINIT);
-static DEFINE_MUTEX(state_lock);
-static struct flag_key secrets;
+static struct flag_key *secrets = NULL;
 
 #define FLAG_FLAG2 1
 #define FLAG_FLAG3 2
@@ -71,7 +66,7 @@ static ssize_t mod_write(struct file *file, const char __user *data, size_t size
 	return -EPERM;
 }
 
-static int authenticate(const struct auth_data *auth)
+static int authenticate(const struct auth_data *auth, const unsigned char key[16])
 {
 	struct sha256_state state;
 	unsigned char digest[32];
@@ -79,7 +74,7 @@ static int authenticate(const struct auth_data *auth)
 	if (auth->message_len > sizeof(auth->message))
 		return -EINVAL;
 	sha256_init(&state);
-	sha256_update(&state, secrets.key, sizeof(secrets.key));
+	sha256_update(&state, key, 16);
 	sha256_update(&state, auth->message, auth->message_len);
 	sha256_final(&state, digest);
 	ret = memcmp(digest, auth->digest, 32);
@@ -89,6 +84,7 @@ static int authenticate(const struct auth_data *auth)
 static long mod_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	unsigned long ret;
+	struct flag_key sec, *global, *tmp;
 	struct auth_data auth;
 	struct file_state *state;
 	void *sptr, *mptr, *eptr;
@@ -97,57 +93,77 @@ static long mod_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 
 	switch (cmd) {
 	case CTFMOD_GET_FLAG1:
-		if (atomic_read(&dev_state) != STATE_READY) {
+		rcu_read_lock();
+		global = rcu_dereference(secrets);
+		if (!global) {
+			rcu_read_unlock();
 			ret = -EBUSY;
 			goto finish;
 		}
-		if (copy_to_user((char *)arg, secrets.flag[0], FLAG_LEN)) {
+		memcpy(sec.flag[0], global->flag[0], FLAG_LEN);
+		rcu_read_unlock();
+		if (copy_to_user((char *)arg, sec.flag[0], FLAG_LEN)) {
 			ret = -EFAULT;
 			goto finish;
 		}
 		ret = 0;
 		goto finish;
 	case CTFMOD_GET_FLAG2:
-		if (atomic_read(&dev_state) != STATE_READY) {
-			ret = -EBUSY;
-			goto finish;
-		}
 		if (!test_bit(FLAG_FLAG2, &state->auth)) {
 			ret = -EPERM;
 			goto finish;
 		}
-		if (copy_to_user((char *)arg, secrets.flag[1], FLAG_LEN)) {
+		rcu_read_lock();
+		global = rcu_dereference(secrets);
+		if (!global) {
+			rcu_read_unlock();
+			ret = -EBUSY;
+			goto finish;
+		}
+		memcpy(sec.flag[1], global->flag[1], FLAG_LEN);
+		rcu_read_unlock();
+		if (copy_to_user((char *)arg, sec.flag[1], FLAG_LEN)) {
 			ret = -EFAULT;
 			goto finish;
 		}
 		ret = 0;
 		goto finish;
 	case CTFMOD_GET_FLAG3:
-		if (atomic_read(&dev_state) != STATE_READY) {
-			ret = -EBUSY;
-			goto finish;
-		}
 		if (!test_bit(FLAG_FLAG3, &state->auth)) {
 			ret = -EPERM;
 			goto finish;
 		}
-		if (copy_to_user((char *)arg, secrets.flag[2], FLAG_LEN)) {
+		rcu_read_lock();
+		global = rcu_dereference(secrets);
+		if (!global) {
+			rcu_read_unlock();
+			ret = -EBUSY;
+			goto finish;
+		}
+		memcpy(sec.flag[2], global->flag[2], FLAG_LEN);
+		rcu_read_unlock();
+		if (copy_to_user((char *)arg, sec.flag[2], FLAG_LEN)) {
 			ret = -EFAULT;
 			goto finish;
 		}
 		ret = 0;
 		goto finish;
 	case CTFMOD_AUTHENTICATE:
-		if (atomic_read(&dev_state) != STATE_READY) {
+		rcu_read_lock();
+		global = rcu_dereference(secrets);
+		if (!global) {
+			rcu_read_unlock();
 			ret = -EBUSY;
 			goto finish;
 		}
+		memcpy(sec.key, global->key, 16);
+		rcu_read_unlock();
 		if (copy_from_user(&auth, (void *)arg, sizeof(struct auth_data))) {
 			printk(KERN_INFO "ctfmod: failed to authenticate\n");
 			ret = -EINVAL;
 			goto finish;
 		}
-		ret = authenticate(&auth);
+		ret = authenticate(&auth, sec.key);
 		if (ret)
 			goto finish;
 
@@ -181,30 +197,28 @@ static long mod_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 			ret = -EPERM;
 			goto finish;
 		}
-		if (mutex_lock_interruptible(&state_lock)) {
-			printk(KERN_DEBUG "ctfmod: LOAD_SECRETS failed: could not obtain lock\n");
-			ret = -ERESTARTSYS;
+		tmp = kmalloc(sizeof(struct flag_key), GFP_KERNEL);
+		if (!tmp) {
+			printk(KERN_ERR "ctfmod: LOAD_SECRETS failed: could not allocate memory\n");
+			ret = -ENOMEM;
 			goto finish;
 		}
-		if (atomic_read(&dev_state) == STATE_READY) {
-			printk(KERN_DEBUG "ctfmod: LOAD_SECRETS failed: already initialised\n");
-			ret = -EBUSY;
-			goto rel_state_lock;
-		}
-		if (copy_from_user(&secrets, (void *)arg, sizeof(struct flag_key))) {
+		if (copy_from_user(tmp, (void *)arg, sizeof(struct flag_key))) {
 			printk(KERN_DEBUG "ctfmod: LOAD_SECRETS failed: could not obtain struct\n");
 			ret = -EFAULT;
-			goto rel_state_lock;
+			goto finish;
 		}
-		printk(KERN_DEBUG "ctfmod: loaded flag1: %*pE\n", FLAG_LEN, secrets.flag[0]);
-		printk(KERN_DEBUG "ctfmod: loaded flag2: %*pE\n", FLAG_LEN, secrets.flag[1]);
-		printk(KERN_DEBUG "ctfmod: loaded flag3: %*pE\n", FLAG_LEN, secrets.flag[2]);
-		printk(KERN_DEBUG "ctfmod: loaded key: %*ph\n", 16, secrets.key);
-
-		atomic_set(&dev_state, STATE_READY);
+		printk(KERN_DEBUG "ctfmod: loaded flag1: %*pE\n", FLAG_LEN, tmp->flag[0]);
+		printk(KERN_DEBUG "ctfmod: loaded flag2: %*pE\n", FLAG_LEN, tmp->flag[1]);
+		printk(KERN_DEBUG "ctfmod: loaded flag3: %*pE\n", FLAG_LEN, tmp->flag[2]);
+		printk(KERN_DEBUG "ctfmod: loaded key: %*ph\n", 16, tmp->key);
+		global = rcu_dereference(secrets);
+		rcu_assign_pointer(secrets, tmp);
+		synchronize_rcu();
+		kfree(global);
 		ret = 0;
 		printk(KERN_DEBUG "ctfmod: LOAD_SECRETS succeeded\n");
-		goto rel_state_lock;
+		goto finish;
 	case CTFMOD_RETRIEVE_SECRETS:
 		kuid = make_kuid(current_user_ns(), 0);
 		if (!uid_eq(current_cred()->uid, kuid) && !capable(CAP_SYS_ADMIN)) {
@@ -212,12 +226,17 @@ static long mod_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 			ret = -EPERM;
 			goto finish;
 		}
-		if (atomic_read(&dev_state) != STATE_READY) {
+		rcu_read_lock();
+		global = rcu_dereference(secrets);
+		if (!global) {
+			rcu_read_unlock();
 			printk(KERN_DEBUG "ctfmod: RETRIEVE_SECRETS failed: not initialised\n");
 			ret = -EBUSY;
 			goto finish;
 		}
-		if (copy_to_user((void *)arg, &secrets, sizeof(struct flag_key))) {
+		memcpy(&sec, global, sizeof(struct flag_key));
+		rcu_read_unlock();
+		if (copy_to_user((void *)arg, &sec, sizeof(struct flag_key))) {
 			printk(KERN_DEBUG "ctfmod: RETRIEVE_SECRETS failed: could not write struct\n");
 			ret = -EFAULT;
 			goto finish;
@@ -227,14 +246,14 @@ static long mod_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		printk(KERN_DEBUG "ctfmod: RETRIEVE_SECRETS succeeded\n");
 		goto finish;
 	case CTFMOD_CHECK_STATUS:
-		ret = atomic_read(&dev_state) == STATE_READY ? 0 : -EBUSY;
+		rcu_read_lock();
+		ret = rcu_dereference(secrets) ? 0 : -EBUSY;
+		rcu_read_unlock();
 		goto finish;
 	default:
 		ret = -ENOTTY;
 		goto finish;
 	}
-rel_state_lock:
-	mutex_unlock(&state_lock);
 finish:
 	return ret;
 }
@@ -270,6 +289,8 @@ static int __init mod_init(void)
 
 static void __exit mod_exit(void)
 {
+	if (secrets)
+		kfree(secrets);
 	misc_deregister(&mod_miscdev);
 	printk(KERN_DEBUG "ctfmod: module unloaded\n");
 }
